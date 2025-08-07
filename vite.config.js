82ee90343244af5667e33514f48e3f5ab6f13f7b
Julian <juliancaceres204@gmail.com>
2025-08-07 01:30:33 -0300

    fix vite cfg

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,13 @@ import { resolve } from 'path'
 
 export default defineConfig({
   // Configuración para GitHub Pages
-  base: process.env.NODE_ENV === 'production' ? '/tu-repositorio/' : '/',
-  
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         players: resolve(__dirname, 'players/index.html'),
       }
-    },
-    outDir: 'dist',
+    }
   },
-  
-  server: {
-    open: '/'
-  }
+  base:'https://juliancz-a.github.io/aoe2-tv' 
 })
